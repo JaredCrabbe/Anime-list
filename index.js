3b1e18fb6b58289ee4959400059766a427e7871f
Jared Crabbe <124356249+JaredCrabbe@users.noreply.github.com>
2023-12-29 13:17:48 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const searchBar = document.getElementById("search-bar")
+const searchBtn = document.getElementById("search-btn")
+const seasonal = document.getElementById("recommended-slider")
+const seasonal1 = document.getElementById("slider")
+
+
+const rankingUrl = 'https://anime-db.p.rapidapi.com/anime/by-ranking/';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7036ef53d4mshadbbeb475a3a0c8p144da1jsn718603bed062',
+		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+	}
+};
+
+
+
+
+
+
+async function getRanked(){
+    try {
+        for(let i = 1; i <= 10; i++){
+            const response = await fetch(rankingUrl + `${i}`, options);
+            const result = await response.json();
+            console.log(result);
+            // seasonal.style.backgroundImage = `url(${result["image"]})`
+            // seasonal1.style.backgroundImage = `url(${result["image"]})`
+    }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRanked()
